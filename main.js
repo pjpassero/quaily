32740d59cf444ced79ca0ad690a404368da88dba
Your Name <59211806+Developer1545@users.noreply.github.com>
2022-11-08 10:01:25 -0500

    Updated Main

--- main.js
+++ main.js
@@ -301,7 +301,9 @@ if (isMobile.iOS() || isMobile.Android()) {
     document.getElementsByName("contentModal")[0].style.margin = "5%";
     document.getElementsByClassName("modalHeader")[0].style.fontSize = "3.5em";
     document.getElementsByClassName("answerBtn")[0].style.fontSize = "2.5em";
-} else {
+};
+/*
+ else {
     document.getElementsByTagName('body')[0].style.marginLeft = "0%";
     document.getElementsByTagName('body')[0].style.margin = "5%";
     document.getElementById("titleHeader").style.fontSize = "4em";
@@ -312,3 +314,4 @@ if (isMobile.iOS() || isMobile.Android()) {
     document.getElementsByClassName("modalHeader")[0].style.fontSize = "3.5em";
     document.getElementsByClassName("answerBtn")[0].style.fontSize = "2.5em";
 };
+*/
